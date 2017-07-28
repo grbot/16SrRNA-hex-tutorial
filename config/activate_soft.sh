49927ba1f19e7a4839905a6e4c4873ee73fc09d5
Gerrit Botha <gerrit.botha@uct.ac.za>
2017-07-28 15:22:36 +0200

    Now activate all software in a single config script.
    Enabling QIIME, usearch, additional tools and tutorial specific software.

---
+++ config/activate_soft.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH=/opt/exp_soft/qiime/lib/python2.7/site-packages/:$PYTHONPATH
+export PATH=/opt/exp_soft/qiime/bin/:$PATH
+export PATH=/opt/exp_soft/qiime/python-2.7.3/bin:$PATH
+export RDP_JAR_PATH=/opt/exp_soft/qiime/app/rdp_classifier_2.2/rdp_classifier-2.2.jar
+export PATH=/opt/exp_soft/qiime/R-3.0.2/bin:$PATH
+export PATH=$PATH:/opt/exp_soft/qiime/app/ChimeraSlayer
+export PYRO_LOOKUP_FILE=/opt/exp_soft/qiime/packages/extras/AmpliconNoiseV1.27/Data/LookUp_E123.dat
+export SEQ_LOOKUP_FILE=/opt/exp_soft/qiime/packages/extras/AmpliconNoiseV1.27/Data/Tran.dat
+export SOURCETRACKER_PATH=/opt/exp_soft/qiime/packages/extras/sourcetracker-0.9.5
+export BLASTMAT=/opt/exp_soft/qiime/packages/extras/blast-2.2.22/data
+export DONT_USE_MPI=1
